8836619d74916717794a59903553092dcfafaf28
topfernorbert <norbert.topfer@gmail.com>
2024-07-21 18:28:31 +0200

    proba

--- Configs/config.py
+++ Configs/config.py
@@ -4,7 +4,8 @@ from selenium.webdriver.chrome.options import Options
 def get_preconfigured_chrome_driver() -> webdriver.Chrome:
     options = Options()
     options.add_experimental_option('detach', True)
-    options.add_argument('window-position=2000,50')
+    options.add_argument('--headless')
+    options.add_argument('window_size=1024,800')
     # options.add_argument("--lang=hu")
     options.add_argument("--lang=en")
     # options.add_experimental_option('excludeSwitches', ['enable-logging'])  # Allure certificate error esetén
